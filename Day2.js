1092c302a3902091b1847965ac35145fdb5379f6
Samara <samarabp7@gmail.com>
2022-12-02 11:31:03 -0800

    starting on day 2

---
+++ Day2.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+fs.readFile('Day2Input.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+// enter code here
+// Rules
+// Rock Papper Scissors
+// A X | B Y | C Z
+// 1   | 2   | 3
+//
+// Loose 0 | Draw 3 | Win 6
+
+
+//determin who wins
+
+//Add up winners score
+
+//
+
+
+});
